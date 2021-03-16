064e7e1ea0dff611c390311e62a8cf3a7d687ae7
Weixi779 <1349750521@qq.com>
2021-03-16 12:25:31 +0800

    Initial Commit

---
+++ HomeCenter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeCenter
+//
+//  Created by 孙世伟 on 2021/3/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeCenter/HomeCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCenterApp.swift
+//  HomeCenter
+//
+//  Created by 孙世伟 on 2021/3/16.
+//
+
+import SwiftUI
+
+@main
+struct HomeCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
